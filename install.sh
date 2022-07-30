d05ac4a5d0ebe02c1c73faa04c7ed93b6f993c2b
Muskelo <titanioman@mail.ru>
2022-07-30 19:40:54 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,42 @@
 
 # Install tmux config
 
-# Осторожно, можно потерять текущие конфиги
-ln -sf $HOME/.config/tmux/.tmux.conf.local $HOME/.tmux.conf.local
+# data ------------>
+
+# constant
+PROGNAME=$(basename $0)
+# variables
+config_path="$HOME/.config/tmux"
+repo_url=
+
+# functions ---------->
+
+usage () {
+    echo "$PROGNAME: usage: $PROGNAME [--path config_path]"
+}
+
+handle_argumets (){
+    while [[ -n $1 ]]; do
+        case $1 in
+            --path) config_path=$2
+                shift
+                ;;
+            -h) usage
+                exit
+                ;;
+            *)  usage
+                exit 1
+                ;;
+        esac
+        shift
+    done
+}
+
+# script --------------->
+
+echo "Старт автонастройки tmux."
+
+handle_argumets
 
 # install oh my tmux
 if ! [ -d "$HOME/.tmux"]
@@ -11,3 +45,6 @@ then
     git clone https://github.com/gpakosz/.tmux.git $HOME/.tmux
     ln -s -f $HOME/.tmux/.tmux.conf  $HOME/.tmux.conf
 fi
+
+# create symlink to config
+ln -sf $install_path/.tmux.conf.local $HOME/.tmux.conf.local
